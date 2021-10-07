8b15229d2391b10d9ca331a854ef589dbb719d6c
irikras <irikras@mail.ru>
2021-10-07 16:14:32 +0300

    Tests updated

--- src/test/java/ru/netology/domain/CardOrderTest.java
+++ src/test/java/ru/netology/domain/CardOrderTest.java
@@ -1,40 +1,13 @@
 package ru.netology.domain;
 
-import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Condition.cssValue;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class CardOrderTest {
-    private WebDriver driver;
-
-    @BeforeAll
-    static void setUpAll() {
-        //System.setProperty("webdriver.chrome.driver", "C:\\tmp\\chromedriver.exe");
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-    }
-
-    @AfterEach
-    void teardown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
 
     @Test
     void shouldSubmitRequest() {
@@ -67,12 +40,12 @@ public class CardOrderTest {
     }
 
     @Test
-    void shouldNotSendIncorrectCheckbox() {
+    void shouldNotSendIfIncorrectCheckbox() {
         open("http://localhost:9999");
         $("[data-test-id=name] input").setValue("Иванов Василий");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[type=button").click();
-        $(".checkbox__text").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(".input_invalid").shouldHave(cssValue("color", "rgba(255, 92, 92, 1)"));
     }
 
     @Test
